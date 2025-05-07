86f6dcb129e3ac1351c45a8a940e3a2f9a82436e
sherly-23 <sherlyyamarthy@gmail.com>
2025-05-07 13:58:57 +0530

    emp java file

---
+++ Emp.java
@@ -0,0 +1,22 @@
+public class Emp 
+{
+    String name;
+    int id;
+    double salary;
+    public Emp(String name,int id,double salary)
+    {
+        this.name=name;
+        this.id=id;
+        this.salary=salary;
+    }
+    public void displayInfo()
+    {
+        System.out.println("Employee Id:"+id);
+        System.out.println("Employee Name:"+name);
+        System.out.println("Salary:"+salary);
+    }
+    public static void main(String[] args) {
+            Emp emp1=new Emp("xyz",101,50000);
+            emp1.displayInfo();
+    }
+}
